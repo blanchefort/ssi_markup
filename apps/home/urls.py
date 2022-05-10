403464568412c7160a23fc5c1b937867a27f255b
Igor Shatalin <shatalin.ip@gmail.com>
2022-05-10 21:04:10 +0300

    bf

--- apps/home/urls.py
+++ apps/home/urls.py
@@ -3,7 +3,7 @@
 Copyright (c) 2019 - present AppSeed.us
 """
 
-from django.urls import path, re_path
+from django.urls import path
 from apps.home import views
 
 urlpatterns = [
@@ -16,8 +16,5 @@ urlpatterns = [
     path('rules', views.rules, name='rules'),
     # upload
     path('upload', views.upload, name='upload'),
-
-    # Matches any html file
-    re_path(r'^.*\.*', views.pages, name='pages'),
 
 ]
